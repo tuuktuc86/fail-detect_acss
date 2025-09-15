38ab7388a59509820870368ee74a7b78ac1ff585
yechanlee <tuuktuc86@kaist.ac.kr>
2025-09-15 09:24:00 +0000

    before create agent if visualization problem happens return to here

--- day3/online_pickandrelease.py
+++ day3/online_pickandrelease.py
@@ -6,88 +6,41 @@ from scipy.spatial.transform import Rotation as R
 from torchvision import transforms as T
 from torchvision.transforms import functional as F
 import gymnasium as gym
-
-
-# Isaac Lab 관련 라이브러리 임포트
-from isaaclab.app import AppLauncher
-
-
-# Argparse로 CLI 인자 파싱 및 Omniverse 앱 실행
-parser = argparse.ArgumentParser(description="Tutorial on creating an empty stage.")
-parser.add_argument(
-    "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
-)
-
-AppLauncher.add_app_launcher_args(parser)
-args_cli = parser.parse_args()
-app_launcher = AppLauncher(args_cli)
-simulation_app = app_launcher.app
-
-from isaaclab_tasks.utils.parse_cfg import parse_env_cfg
-from isaaclab.managers import SceneEntityCfg
-
-
-# 커스텀 환경 시뮬레이션 환경 config 파일 임포트
-from task.lift.custom_pickplace_env_cfg_3_3 import YCBPickPlaceEnvCfg
-
-# gymnasium 라이브러리를 활용한 시뮬레이션 환경 선언
-from task.lift.config.ik_abs_env_cfg_3_3 import FrankaYCBPickPlaceEnvCfg
-gym.register(
-    id="Isaac-Lift-Cube-Franka-Custom-v0",
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    kwargs={
-        "env_cfg_entry_point": FrankaYCBPickPlaceEnvCfg,
-    },
-    disable_env_checker=True,
-)
+import set_env
+from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
+from skrl.envs.loaders.torch import load_isaaclab_env
+from skrl.envs.wrappers.torch import wrap_env
+from skrl.memories.torch import RandomMemory
+from skrl.models.torch import DeterministicMixin, GaussianMixin, Model
+from skrl.resources.preprocessors.torch import RunningStandardScaler
+from skrl.resources.schedulers.torch import KLAdaptiveLR
+from skrl.trainers.torch import SequentialTrainer
+from skrl.utils import set_seed
 
 
 def main():
     """메인 함수"""
     # 환경 갯수(1개로 고정)
-    num_envs = 2
-
-    # 환경 및 설정 파싱
-    env_cfg: YCBPickPlaceEnvCfg = parse_env_cfg(
-        "Isaac-Lift-Cube-Franka-Custom-v0",
-        device=args_cli.device,
-        num_envs=num_envs,
-        use_fabric=not args_cli.disable_fabric,
-    )
-
-    # 환경 생성 및 초기화
-    env = gym.make("Isaac-Lift-Cube-Franka-Custom-v0", cfg=env_cfg)
-
     
+
+    env = set_env.make_env()
     obs, _ = env.reset()
-
+    device = env.device
     print(f"Environment reset. Number of environments: {env.unwrapped.num_envs}")
-    
-    # 환경 관측 카메라 시점 셋팅
 
-    from isaacsim.core.api.objects import DynamicCuboid
-    from isaacsim.core.api import World
-    import isaacsim.core.utils.numpy.rotations as rot_utils
-    import numpy as np
-    import matplotlib.pyplot as pl
-
-    # 환경 연산 디바이스(gpu)
-    device = env.unwrapped.scene.device
-    max_trajectories = 5
-    total_traj = 0 
-    max_steps_per_traj = 300
+    # # 환경 연산 디바이스(gpu)
+    # device = env.unwrapped.scene.device
+    # max_trajectories = 5
+    # total_traj = 0 
+    # max_steps_per_traj = 300
 
     # actions = torch.tensor(
-    #     [[0.3960, -0.0809, 0.3323, -0.0573, 0.9845, 0.1090, 0.1252, -1.0000]],
+    #     [[0.4000, 0.0000, 0.200, -0.0573, 0.9845, 0.1090, 0.1252, 1.0000]],
     #     device="cuda:0"
     # )
-    actions = torch.tensor(
-        [[0.4000, 0.0000, 0.200, -0.0573, 0.9845, 0.1090, 0.1252, 1.0000]],
-        device="cuda:0"
-    )
     # 시뮬레이션 루프
-    while simulation_app.is_running() and total_traj < max_trajectories:
-        env.reset()
+    while 1:
+        
         print(f"[INFO] Starting trajectory {total_traj+1}/{max_trajectories}")
         
         done = False
---
+++ day3/ppo.py
@@ -0,0 +1,44 @@
+# define shared model (stochastic and deterministic models) using mixins
+# import the skrl components to build the RL system
+from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
+from skrl.envs.loaders.torch import load_isaaclab_env
+from skrl.envs.wrappers.torch import wrap_env
+from skrl.memories.torch import RandomMemory
+from skrl.models.torch import DeterministicMixin, GaussianMixin, Model
+from skrl.resources.preprocessors.torch import RunningStandardScaler
+from skrl.resources.schedulers.torch import KLAdaptiveLR
+from skrl.trainers.torch import SequentialTrainer
+from skrl.utils import set_seed
+class Shared(GaussianMixin, DeterministicMixin, Model):
+    def __init__(self, observation_space, action_space, device, clip_actions=False,
+                 clip_log_std=True, min_log_std=-20, max_log_std=2, reduction="sum"):
+        Model.__init__(self, observation_space, action_space, device)
+        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
+        DeterministicMixin.__init__(self, clip_actions)
+
+        self.net = nn.Sequential(nn.Linear(self.num_observations, 256),
+                                 nn.ELU(),
+                                 nn.Linear(256, 128),
+                                 nn.ELU(),
+                                 nn.Linear(128, 64),
+                                 nn.ELU())
+
+        self.mean_layer = nn.Linear(64, self.num_actions)
+        self.log_std_parameter = nn.Parameter(torch.ones(self.num_actions))
+
+        self.value_layer = nn.Linear(64, 1)
+
+    def act(self, inputs, role):
+        if role == "policy":
+            return GaussianMixin.act(self, inputs, role)
+        elif role == "value":
+            return DeterministicMixin.act(self, inputs, role)
+
+    def compute(self, inputs, role):
+        if role == "policy":
+            self._shared_output = self.net(inputs["states"])
+            return self.mean_layer(self._shared_output), self.log_std_parameter, {}
+        elif role == "value":
+            shared_output = self.net(inputs["states"]) if self._shared_output is None else self._shared_output
+            self._shared_output = None
+            return self.value_layer(shared_output), {}
